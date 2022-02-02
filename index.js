b9629971288e8685977a6d05e933ff4061347c5b
Loka Hell <xifere2807@porjoton.com>
2022-02-02 18:30:31 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const exp = require('express');
+const path = require('path/posix');
 
 const app = exp();
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('Listing at port 5000'))
 app.use(exp.static('public'))
+app.use(exp.static(path.join(__dirname,'public')))
