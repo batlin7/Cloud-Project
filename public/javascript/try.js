3950008489d89ea148fbbb1b99b4800b63e1aeac
Loka Hell <xifere2807@porjoton.com>
2022-02-02 22:37:40 +0530

    make it better

--- public/javascript/try.js
+++ public/javascript/try.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("span").click(function(){
-      alert(atob("Q29uZ3JhdHVsYXRpb25zISBmb2xsb3cgeW91ciBsZWFkOiBodHRwczovL3d3dy55b3V0dWJlLmNvbS93YXRjaD92PWRRdzR3OVdnWGNRJmFiX2NoYW5uZWw9Umlja0FzdGxleQ=="));
+      alert(atob('Q29uZ3JhdHVsYXRpb25zISBmb2xsb3cgeW91ciBsZWFkOiBodHRwczovL3d3dy55b3V0dWJlLmNvbS93YXRjaD92PWRRdzR3OVdnWGNRJmFiX2NoYW5uZWw9Umlja0FzdGxleQ=='));
       main().catch(console.error);
     });
   });
