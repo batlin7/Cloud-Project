2be7b8bb3f1e4f3e009ac73c4131fa7e2beb7dbb
Loka Hell <xifere2807@porjoton.com>
2022-02-02 23:38:27 +0530

    make it better

--- index.js
+++ index.js
@@ -8,4 +8,3 @@ const port = process.env.PORT || 5000
 app.listen(port, () => console.log('Listing at port 5000'))
 app.use(exp.static('public'))
 app.use(exp.static(path.join(__dirname,'public')))
-app.use(ip)
