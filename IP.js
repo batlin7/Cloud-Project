cc859bc100a5844e35e50bd2ca1c0d800844e665
Loka Hell <xifere2807@porjoton.com>
2022-02-02 23:27:08 +0530

    make it better

---
+++ IP.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require('mongodb');
+const fetch = require('node-fetch');
+
+var datetime = new Date();
+
+async function main() {
+
+  const uri = "mongodb+srv://test1hello:3rZzGLm4dimFHam@testdb-01.oupbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      //Get client IP address
+      const C_ID = await clientID();
+      console.log(C_ID);
+
+      await createnewdata(client, {
+        User_ID : C_ID,
+        date : datetime
+        })
+      } catch (e) {
+      console.error(e);
+  } finally {
+      // Close the connection to the MongoDB cluster
+      await client.close();
+  }
+  alert("Done");
+}
+
+//Client IP address function
+async function clientID(){
+const response = await fetch('http://ip-api.com/json');
+const data = await response.json();
+
+return data;
+}
+
+//Insert Data into DB one by one
+async function createnewdata(client, newListing){
+const result = await client.db("CentipedeUser").collection("Players").insertOne(newListing);
+
+console.log(`Inserted Id: ${result.insertedId}`);
+}
+
+main().catch(console.error);
