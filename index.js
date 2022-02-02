62be01ed43e28fa84b1408519f464f2404d965a1
Loka Hell <xifere2807@porjoton.com>
2022-02-02 23:47:52 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const exp = require('express');
 const path = require('path/posix');
-const adds = require('IP.js');
+const { MongoClient } = require('mongodb');
+const fetch = require('node-fetch');
 
 const app = exp();
 const port = process.env.PORT || 5000
@@ -8,4 +9,49 @@ const port = process.env.PORT || 5000
 app.listen(port, () => console.log('Listing at port 5000'))
 app.use(exp.static('public'))
 app.use(exp.static(path.join(__dirname,'public')))
-app.use(adds)
+
+
+var datetime = new Date();
+
+async function main() {
+
+  const uri = "mongodb+srv://test1hello:3rZzGLm4dimFHam@testdb-01.oupbs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      //Get client IP address
+      const C_ID = await clientID();
+      console.log(C_ID);
+
+      await createnewdata(client, {
+        User_ID : C_ID,
+        date : datetime
+        })
+      } catch (e) {
+      console.error(e);
+  } finally {
+      // Close the connection to the MongoDB cluster
+      await client.close();
+  }
+  alert("Done");
+}
+
+//Client IP address function
+async function clientID(){
+const response = await fetch('http://ip-api.com/json');
+const data = await response.json();
+
+return data;
+}
+
+//Insert Data into DB one by one
+async function createnewdata(client, newListing){
+const result = await client.db("CentipedeUser").collection("Players").insertOne(newListing);
+
+console.log(`Inserted Id: ${result.insertedId}`);
+}
+
+main().catch(console.error);
