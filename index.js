a13c127d7505c5082068129a0742778d398f2ff4
Loka Hell <xifere2807@porjoton.com>
2022-02-22 23:14:08 +0530

    make it better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function key(phs) {
 const app = exp();
 const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log('Listing at port 5000'))
+app.listen(port, () => console.log('Listing at port'+port ))
 
 //Home Page
 app.use(exp.static('public'))
